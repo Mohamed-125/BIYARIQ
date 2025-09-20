50b7e4f041cf1efc845235efce410bac07379592
Mohamed-125 <mohamedibrahim.fed@example.com>
2025-09-20 20:51:55 +0300

    improving affilate daschboard

--- next.config.js
+++ next.config.js
@@ -3,8 +3,7 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  // تعطيل التخزين المؤقت في وضع التطوير
-  cache: process.env.NODE_ENV === 'development' ? false : true,
+  // cache: process.env.NODE_ENV === 'development' ? false : true,
 };
 
 module.exports = nextConfig;
